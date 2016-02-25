bfb0807ef02302264582a190ea32347e53773990
pmmk.oktat <tamas.pinter@suitsolutions.eu>
2016-02-25 12:04:47 +0100

    tag

---
+++ src/hu/sol/mik/hw/test/taglib/HelloTag.java
@@ -0,0 +1,24 @@
+package hu.sol.mik.hw.test.taglib;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.JspFragment;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class HelloTag extends SimpleTagSupport {
+
+	@Override
+	public void doTag() throws JspException, IOException {
+		JspFragment jspBody = getJspBody();
+		StringWriter sw = new StringWriter();
+		jspBody.invoke(sw);
+		JspWriter out = getJspContext().getOut();
+		out.print("Hello ");
+		out.print(sw);
+		out.print("!");
+	}
+
+}
