ddf9bf41cd0b7125c8931f229ee563ff81b8c224
pmmk.oktat <tamas.pinter@suitsolutions.eu>
2016-02-25 12:02:52 +0100

    Első óra, helloWorld

---
+++ src/hu/sol/mik/hw/test/bean/HelloBean.java
@@ -0,0 +1,15 @@
+package hu.sol.mik.hw.test.bean;
+
+public class HelloBean {
+	private String sayHello = "Hello Tamás!";
+
+	public String getSayHello() {
+		return sayHello;
+	}
+
+	public void setSayHello(String sayHello) {
+		this.sayHello = sayHello;
+	}
+	
+	
+}
---
+++ src/hu/sol/mik/hw/test/servlet/HelloServlet.java
@@ -0,0 +1,31 @@
+package hu.sol.mik.hw.test.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		PrintWriter writer = resp.getWriter();
+		writer.print("Hello MIK!");
+		writer.flush();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+	}
+
+	
+	
+	
+}
